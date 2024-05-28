e471f78bf0e186feead557f1ec2d6c8854d48c37
adalynn133 <169125708+adalynn133@users.noreply.github.com>
2024-05-28 13:21:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import csv
 app = Flask(__name__)
 
 # 設置 Line Bot 的 Channel access token 和 Channel secret
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi('CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('CHANNEL_SECRET')
 
 @app.route("/callback", methods=['POST'])
 def callback():
